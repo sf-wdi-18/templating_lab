ceaed8eabe7b23d85e61c4486e0dff3d94c42d46
nathanallen <thenathanator@gmail.com>
2015-05-13 13:33:26 -0700

    solution rough

--- public/js/app.js
+++ public/js/app.js
@@ -1,43 +1,38 @@
 // on page load
 $(function(){
 
-  //problem1("footer", "YOUR NAME")
-  //problem2()
-  //problem3()
-  //problem4()
-  //problem5()
+  // problem1("footer", "Nathan Allen")
+  // problem2("footer", 2015)
+  // problem3("h1 small", "Git in muh belluh!")
+  // problem4()
+  problem5("h1", "http://media1.giphy.com/media/GsZDu04MCYEg/100.gif")
   //problem6()
+
+
   
+  var myData = {name: "Nathan", list: ["hello", "world"]}
+  var tmpl_str = $("#tmpl-loop-example").html();
+  var compile = _.template(tmpl_str); // returns a function!
+  var html_str = compile(myData);
+  $("body").html(html_str)
+
+
 });
 
 // // // // // // //
 
-function problem1(targetEl, name){
-  // using an html string + jQuery
-  // overwrite "Created by Jane Doe" in the footer
-  // replace it with your name
-}
-function problem2(targetEl, year){
-  // append "2015" to the footer inside a `small` tag
-}
-function problem3(targetEl, tagline){
-    // console.log the html of the old tagline
-    // change "Put it in my belly!" to "Get in my belly"
+function problem5(targetEl, gif_url){
+  var tmpl_str = "<img src='<%= url %>'>";
+  var compile = _.template(tmpl_str); // returns a function!
+  var html_str = compile({url: gif_url});
+  $(targetEl).prepend(html_str);
 }
 
 function problem4(){
-  // now we're going to use underscore!
-  // console.log("templateString:", "<p><%= YOUR_KEY ></p>")
-  // Let's look at the docs: http://underscorejs.org/#template
-  // console.log the interpolated html string returned by the `template` method
-  // it should look like "<p>Hello!</p>"
-}
-
-function problem5(targetEl, gif_url){
-  // Using underscore templating and the following gif
-  // "http://media1.giphy.com/media/GsZDu04MCYEg/100.gif"
-  // Create an img element and set its src attribute to the gif
-  // Prepend the gif to the header.
+  var tmpl_str = "<p><%= whatevs %></p>"
+  var compile = _.template(tmpl_str); // returns a function!
+  var html_str = compile({whatevs: "Hello!"})
+  console.log(html_str) // "<p>Hello!</p>"
 }
 
 function problem6(targetId, templateId, foodname){
@@ -49,9 +44,7 @@ function problem6(targetId, templateId, foodname){
   // add the new html to the targrt ul
 }
 
-
 var foods7 = ["Sushiritto", "Green Eggs & Ham", "Crayfish", "Foie Gras", "Taco", "Kale"];
-
 function problem7(targetId, templateId, foods){
   // Using your solution to #6 and a loop,
   // can you append each food item from the foods list above to the ul?
@@ -69,4 +62,18 @@ var foods8 =[
 function problem8(targetId, templateId, first_food){
   // Using your solution to #6, wihthout making major changes,
   // can you render just the first item, "Sushiritto", in your list?
+}
+
+function problem1(targetEl, name){
+  $(targetEl).text("Created by " + name);
+}
+
+function problem2(targetEl, year){
+  $(targetEl).append("<small>" + year + "</small>")
+}
+
+function problem3(targetEl, tagline){
+    var old_html = document.querySelector(targetEl).outerHTML
+    console.log(old_html)
+    $(targetEl).text(tagline)
 }
