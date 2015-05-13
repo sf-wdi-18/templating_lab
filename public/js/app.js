848640a9d66b06bf6eeb06d190f955f0b37a0a7e
Ilias Tsangaris <iliastsangaris@gmail.com>
2015-05-12 22:40:22 -0700

    nom nom nom

--- public/js/app.js
+++ public/js/app.js
@@ -2,24 +2,12 @@
 $(function(){
   // get and render the food
   Food.all();
-  // set the view's behaviors
-  View.init();
 });
 
 // // // // // // //
 
 // VIEW OBJECT
 function View() {};
-View.init = function() {
-  // food form submit event listener
-  $("#food-form").on("submit", function(e){
-    // stop page reload
-    e.preventDefault();
-    // format form data into a query string
-    var foodParams = $(this).serialize();
-    Food.create(foodParams);
-  });
-}
 View.render = function(items, parentId, templateId) {
   // render a template
   var template = _.template($("#" + templateId).html());
@@ -36,21 +24,4 @@ Food.all = function() {
     // render the results
     View.render(foods, "food-ul", "foods-template");
   });
-}
-Food.create = function(foodParams) {
-  $.post("/foods", foodParams).done(function(res){
-    // once done, re-render all foods
-    Food.all();
-  });
-}
-Food.delete = function(food) {
-  var foodId = $(food).data().id;
-  $.ajax({
-    url: '/foods/' + foodId,
-    type: 'DELETE',
-    success: function(res) {
-      // once successfull, re-render all foods
-      Food.all();
-    }
-  })
 };
